4e31b0750b3a4a26b4c3812296a9ebe3a52d4a1c
StephSnopek <stephaniesnopek@gmail.com>
2015-01-21 15:08:42 -0800

    added sweet alerts library

--- main.js
+++ main.js
@@ -12,24 +12,58 @@ console.log(motivation1)
 
 var motivation2 = {};
 
-motivation2.quote = "You're awesome"
+motivation2.quote = "You're AWESOME!!!"
 motivation2.oHigh= "Yeah I am!"
-motivation2.oMed = "continue"
+motivation2.oMed = "Continue"
 motivation2.oLow = "I don't feel awesome..."
+
+var motivation3 = {};
+
+motivation3.quote = "You are super smart!"
+motivation3.oHigh= "Yeah I am!"
+motivation3.oMed = "Continue"
+motivation3.oLow = "I don't know about that..."
 
 console.log(JSON.stringify(motivation2))
 
-motivationArray.push(motivation1, motivation2)
 
-function onBodyLoad(){
-  console.log("bodyLoad")
+motivationArray.push(motivation1, motivation2, motivation3)
+//need to make the randomizer understand not to choose the same item twice in a row
+var currentQuote = '';
+function nextQuote(){
+  var result = motivationArray[Math.floor(Math.random()*motivationArray.length)];
+  console.log(currentQuote)
+  while(result.quote === currentQuote){
+    result = motivationArray[Math.floor(Math.random()*motivationArray.length)];
+    console.log('still looking')
+  }
+  currentQuote = result.quote
+
   var quoteContainer = document.querySelector("#quote");
   var oHighContainer= document.querySelector("#oHigh");
   var oMedContainer = document.querySelector("#oMed");
   var oLowContainer = document.querySelector("#oLow");
 
-  quoteContainer.innerHTML = motivationArray[0].quote;
-  oHighContainer.innerHTML = motivationArray[0].oHigh;
-  oMedContainer.innerHTML = motivationArray[0].oMed;
-  oLowContainer.innerHTML = motivationArray[0].oLow;
+  quoteContainer.innerHTML = result.quote;
+  oHighContainer.innerHTML = result.oHigh;
+  oMedContainer.innerHTML = result.oMed;
+  oLowContainer.innerHTML = result.oLow;
+
+  console.log(result)
+
 }
+
+function happyClick () {
+  swal('Yeah you are! Now go do some fucking work!')
+
+}
+
+function onBodyLoad(){
+  nextQuote()
+
+}
+/*document.querySelector("#oHigh").onClick('swal({
+  title: "Error!",
+  text: "Here is my error message!",
+  type: "error",
+  confirmButtonText: "Cool"})')*/
