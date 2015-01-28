fbb971876a2a37ef701ef2a5af6d5da11afb15e5
StephSnopek <stephaniesnopek@gmail.com>
2015-01-27 17:30:40 -0800

    edited sadClick alert to fit mobile

--- bundle.js
+++ bundle.js
@@ -40,13 +40,12 @@ function happyClick () {
 
   swal({
     title: "That's more LIKE it!",
-    text: "Now go do some fucking work",
-    button: "hell yes",})
+    text: "Now go do some fucking work",})
 
 }
 
 function sadClick(){
-  swal('Hey, we all have bad days. Maybe you could work on a passion project?')
+  swal('Maybe you could work on a passion project?')
 
   return nextQuote()
 }
